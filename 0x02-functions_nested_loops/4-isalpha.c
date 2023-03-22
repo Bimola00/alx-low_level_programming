6e18814b06481ff00ddc466663beffe7e54c2332
Bimola00 <bimolaope@gmail.com>
2023-03-21 17:44:34 -0700

    number 3

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - a function that checks for alphabet character
+ * @c: single letter
+ * Return: 1 if c a letter (lowercase or uppercare), 0 otherwise
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 'a' && c <= 'z)) || ((c >= 'A' && c <= 'Z')))
+	if (((c >= 'a' && c <= 'z')) || ((c >= 'A' && c <= 'Z')))
+
+		return (1);
+	else
+		return (0);
+}
