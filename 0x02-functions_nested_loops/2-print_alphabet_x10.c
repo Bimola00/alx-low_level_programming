4992cb443976f545609d207f4416a2087d9ac15a
Bimola00 <bimolaope@gmail.com>
2023-03-21 17:13:23 -0700

    third

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Prints alphabet 10 times, lowercase
+ */
+void print_alphabet_x10(void)
+{
+	int count = 0;
+	char alpha;
+
+	for (count = 0; count <= 10; count++)
+	for (count = 0; count <= 9; count++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+	}
+}
