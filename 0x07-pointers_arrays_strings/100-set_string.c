fce2f5d979fbb199d26077529806ff7470811138
Bimola00 <bimolaope@gmail.com>
2023-04-04 15:55:16 -0700

    set

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * set_string - sets string to pointer char
+ *
+ * @s: pointer to pointer
+ * @too: pointer char
+ */
+
+void set_string(char **s, char *too)
+
+{
+	*s = too;
+}
