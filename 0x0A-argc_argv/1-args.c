33a7a4bdc636939024457eb2eeb4dacc83dd818c
Bimola00 <bimolaope@gmail.com>
2023-04-10 12:48:49 -0700

    args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - starting point
+ *
+ * @argv: vector arg
+ *
+ * @argc: argument count
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+
+	printf("%d\n", argc - 1);
+	return (0);
+}
