7bd6d7c42f13682075c6b32820dbb933c440c94a
Bimola00 <bimolaope@gmail.com>
2023-04-09 17:56:05 -0700

    0 task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - starting point
+ * @argc - arg count
+ * @argv - arg vector
+ * print - my name
+ * Return - Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void) argc;
+	printf("%s\n", argv[0]);
+	printf("My name is bimola[]\n");
+
+
+	return (0);
+}
