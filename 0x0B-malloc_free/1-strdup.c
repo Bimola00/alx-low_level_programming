638081226f7eef5152eaa86b9067eb359353c8bd
Bimola00 <bimolaope@gmail.com>
2023-04-12 15:40:43 -0700

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - copy string
+ *
+ * @str: string
+ *
+ * Return: duplicated string or null if NULL
+ */
+
+char *_strdup(char *str)
+{
+	unsigned int i, j;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		;
+	i++;
+	s = malloc(i * sizeof(char));
+	if (s == NULL)
+	{
+		return (NULL);
+	}
+	for (j = 0; j < i; j++)
+	{
+		s[j] = str[j];
+	}
+	return (s);
+}
