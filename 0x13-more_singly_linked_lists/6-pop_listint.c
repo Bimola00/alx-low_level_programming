e1812e8c3ce807eed86edadb6856878d9f0b2b59
Bimola00 <bimolaope@gmail.com>
2023-05-01 05:41:59 -0700

    task 6

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * pop_listint - function that return the first element in the list
+ * @head: pointer to the first element on the list
+ * Return: the element of the deleted node
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int num;
+
+	if (!(*head != NULL))
+		return (0);
+
+	*head = temp->next;
+	num = temp->n;
+
+	free(temp);
+	return (num);
+}
