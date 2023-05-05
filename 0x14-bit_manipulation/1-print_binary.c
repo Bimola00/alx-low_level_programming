46d81e727c0d4ae63fefcf16c22d8ffa9d608cd9
Bimola00 <bimolaope@gmail.com>
2023-05-04 17:30:14 -0700

    task 2

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * binary_len- function that calc many digit is needed to call the bi num
+ * @n: the num we want to calc
+ * Return: the len
+ */
+int binary_len(unsigned long int n)
+{
+	int len = 0;
+
+	while (n > 0)
+	{
+		len++;
+		n >>= 1;
+	}
+	len--;
+	return (len);
+}
+
+/**
+ * print_binary - function that convert decimal to binary
+ * @n:num we want to convert
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int digit = 1;
+
+	if (binary_len(n) > 0)
+		digit <<= binary_len(n);
+
+	while (digit > 0)
+	{
+		if (n & digit)
+			_putchar('1');
+		else
+			_putchar('0');
+
+		digit >>= 1;
+	}
+}
